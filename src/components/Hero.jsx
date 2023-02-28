9a5002f57fa3fb8679120e6f0cc19d34b3ce5ac9
dwhyq <dwhyq@outlook.com>
2023-02-28 14:50:25 +0100

    Fixed responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
           </h1>
           <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
             {" "}
-            <span className="text-orange-500">Foods </span>Delivered
+            <span className="text-orange-500">Treats </span>Delivered
           </h1>
         </div>
         <img className="w-full max-h-[500px] object-cover" src="https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=900&t=st=1676934848~exp=1676935448~hmac=d9255a8fcf5bbfa847e7149b0ddacba2833d42074570ee233348e76e41a510f6" alt="/" />
